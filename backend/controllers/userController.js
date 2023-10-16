9c4b58ff64dfcfc5f7b35700c27f7b4d56686fee
missaoui ala <120734604+declaredas-ala@users.noreply.github.com>
2023-10-16 13:40:15 +0100

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,25 +5,33 @@ import User from '../models/userModel.js';
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
 // @access  Public
+// @desc    Auth user & get token
+// @route   POST /api/users/auth
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
 
-  const user = await User.findOne({ email });
+    if (!user || user.password !== password) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
 
-  if (user && (await user.matchPassword(password))) {
+    // Generate and set the token as an HTTP-Only cookie using the generateToken function
     generateToken(res, user._id);
 
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin,
-    });
-  } else {
-    res.status(401);
-    throw new Error('Invalid email or password');
+      isAdmin: Boolean(user.isAdmin),
+     });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
+
+
 
 // @desc    Register a new user
 // @route   POST /api/users
